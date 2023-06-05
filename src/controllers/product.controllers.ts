9ed8853973fb7cce63e33f4e2f0e4e2690aa726e
juancamilobanguero <banguerojuancamilo58@gmail.com>
2023-06-05 09:51:52 -0500

    camilocgdj

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -32,40 +32,4 @@ export const createproductos = async (req: Express.Request, res: Express.Respons
      }
  }
 
- export const getfactura = async (req: Express.Request, res: Express.Response) =>{
-     try { 
-          const idProductos = req.params.idProductos;
-          const productos = (idProductos);
-
-          if (productos.length === 0) {
-               return res.status(404).json({ error: 'No se encontraron productos' });
-             }
-             
-             //Calcular el total de la factura sumando los precios de los productos
-             const total = productos.((acc, producto) => acc + producto.precio, 0);
-
-             const factura = {
-               idProductos: idProductos,
-               productos: productos,
-               total: total
-             };
-              // Devolver el resultado final de la factura
-               res.json(factura);
-
-               function obtenerProductosPorIds(idProductos){
-                    const productos = [
-                         { id: 1, nombre: 'Producto 1', precio: 10 },
-                         { id: 2, nombre: 'Producto 2', precio: 15 },
-                         { id: 3, nombre: 'Producto 3', precio: 20 }
-                       ];
-                       const ids = idProductos.split(',');
-
-                       return productos.filter(producto => ids.includes(producto.id.toString()));
-               }
-     } catch (error) {
-         return res.status(400).json({msg: "no se encuentra el precio del producto",error})
-     }
- }
-
-
-
+ 
